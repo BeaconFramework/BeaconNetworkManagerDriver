80f9ed357b8832a690e6ba373811c77bc7497ef1
Giuseppe <giu.tricomi@gmail.com>
2017-07-13 14:49:10 +0200

    intermediate steps

--- src/java/API/SOUTHBR/test.java
+++ src/java/API/SOUTHBR/test.java
@@ -154,8 +154,11 @@ public class test {
         m.connectLocale(m.getMdbIp());
         //qui si stà condividendo la stessa table con entrambe le cloud quindi si dovrà memorizzare la stessa tabella per entrambe le cloud federate. 
         ////Il caso genericon prevede che si inserisca per ogni sito una tabella diversa che può ontenere anche riferimenti precedentemente presenti riferiti a link verso altre cloud
+ //06/07/2017 gt: verificare cquali funzioni usare al posto di queste due
+ /*       
         m.insertNetTable(t1name, body);
         m.insertNetTable(t2name, body);
+ */
             System.out.println(body);
         Response r=fan1.createNetTable(t1Id, "10.9.240.21:4567", body);
             System.out.println(r.toString());
--- src/java/MDBInt/DBMongo.java
+++ src/java/MDBInt/DBMongo.java
@@ -36,11 +36,14 @@ import java.util.NoSuchElementException;
 import org.jdom2.Element;
 import utils.*;
 import MDBInt.MDBIException;
+import org.json.JSONException;
 import com.mongodb.AggregationOutput;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 /**
@@ -290,7 +293,7 @@ public class DBMongo {
     }
     
     /**
-     * This update Federation User with element. 
+     * This update Federation User with element. Used by BNA
      * @param dbName
      * @param tableName
      * @param faSite, this is the cloud Id
@@ -307,11 +310,28 @@ public class DBMongo {
         obj.append("version", version);
         collezione.save(obj);
     }
+    /**
+     * This function save NetTable provided to BNA inside the collection netTables. Used by BNA
+     * @param tenant
+     * @param idcloud, this is the cloud Id
+     * @param jsonTable
+     * @return 
+     */
+    public boolean insertNetTable(String tenant,String idcloud,String jsonTable){
+        DB dataBase = this.getDB(tenant);
+        DBCollection collezione = this.getCollection(dataBase, "netTables");
+        BasicDBObject obj = (BasicDBObject) JSON.parse(jsonTable);
+        obj.append("referenceSite", idcloud);
+        obj.append("insertTimestamp", System.currentTimeMillis());
+        collezione.save(obj);
+        //this.insert(tenant, "NetTablesInfo", jsonTable);
+        return true;
+    }
      //</editor-fold>
     
     //<editor-fold defaultstate="collapsed" desc="siteTables Management Functions">
     /**
-     * 
+     * Used by BNA
      * @param dbName
      * @param faSite, this is the cloud Id
      * @return 
@@ -382,7 +402,7 @@ public class DBMongo {
     }
     
     /**
-     * This update Federation User with element. 
+     * This update Federation User with element. Table used by BNA
      * @param dbName
      * @param faSite, this is the cloud Id
      * @param docJSON 
@@ -396,12 +416,12 @@ public class DBMongo {
         obj.append("referenceSite", faSite);
         obj.append("insertTimestamp", System.currentTimeMillis());
         collezione.save(obj);
-    }
+    }    
      //</editor-fold>
     //BEACON>>>> Valutare se mantenere questa parte di informazioni su MongoDB
     //<editor-fold defaultstate="collapsed" desc="TenantTables Management Functions">
     /**
-     * 
+     * Used by BNA
      * @param dbName
      * @param faSite, this is the cloud Id
      * @return 
@@ -419,14 +439,13 @@ public class DBMongo {
     }
     
     /**
-     * This update Federation User with element. 
+     * This update Federation User with element. Used by BNA
      * @param dbName
-     * @param tableName
      * @param faSite, this is the cloud Id
      * @param docJSON 
      * @author gtricomi
      */
-    public void insertTenantTables(String dbName,String tableName,String faSite, String docJSON) {
+    public void insertTenantTables(String dbName,String faSite, String docJSON) {
 
         DB dataBase = this.getDB(dbName);
         DBCollection collezione = this.getCollection(dataBase, "TenantTables");
@@ -435,7 +454,46 @@ public class DBMongo {
         obj.append("insertTimestamp", System.currentTimeMillis());
         collezione.save(obj);
     }
-     //</editor-fold>
+    
+    /**
+     * This update Federation User with element. Used by BNM
+     * @param dbName
+     * @param docJSON 
+     * @author gtricomi
+     */
+    public void insertTenantTables(String dbName, String docJSON) {
+
+        DB dataBase = this.getDB(dbName);
+        DBCollection collezione = this.getCollection(dataBase, "FedSDNTenantTables");
+        BasicDBObject obj = (BasicDBObject) JSON.parse(docJSON);
+        obj.append("insertTimestamp", System.currentTimeMillis());
+        collezione.save(obj);
+    }
+    /**
+     * Used by BNM
+     * @param dbName
+     * @param faSite, this is the cloud Id
+     * @return 
+     * @author gtricomi
+     */
+    public String getTenantTables(String dbName) {
+
+        DB database = this.getDB(dbName);
+        DBCollection collection = database.getCollection("FedSDNTenantTables");
+        
+ //04/07/2017 gt: eliminare la sort query, questa collezione dovrebbe avere un solo elemento(nel caso in cui si gestisca solo il tenant di federazione, diversamente se si
+ //gestiscono anche gli user allora si deve aggiungere una variabile tra i parametri della funzione da usare come filtro per la resQuery da usare nella conditionedResearch commentata.
+        BasicDBObject sortQuery=new BasicDBObject("version",-1);
+        try{
+            DBCursor b= collection.find().sort(sortQuery).limit(1);
+            return b.next().toString();
+        }catch(Exception e){
+            LOGGER.error("Conditioned Research for collection: "+collection+", sortQuery "+sortQuery);
+            return null;
+        }
+        //return this.conditionedResearch(collection,resQuery,sortQuery);
+    }
+    //</editor-fold>
     
     public String getRunTimeInfo(String dbName, String uuid) {
 
@@ -449,6 +507,49 @@ public class DBMongo {
         obj = collection.findOne(first);
 
         return obj.toString();
+
+    }
+    public String getRunTimeInfo(String dbName, String idcloud,String uuidTemplate,String stackname) {
+
+        BasicDBObject first = new BasicDBObject();
+        first.put("idCloud", idcloud);
+        first.put("uuidTemplate",uuidTemplate);
+        first.put("stackName",stackname);
+
+        DB database = this.getDB(dbName);
+        DBCollection collection = database.getCollection("runTimeInfo");
+        try{
+            DBCursor b= collection.find(first).sort(new BasicDBObject("insertTimestamp",-1)).limit(1);
+            System.out.println("query: "+first);
+            return b.next().toString();
+        }catch(Exception e){
+            LOGGER.error("Conditioned Research for collection: "+collection+", resQuery , sortQuery ");
+            return null;
+        }
+    }
+
+    public ArrayList<String> getRunTimeInfos(String dbName, String idcloud,String uuidTemplate,String stackname) {
+        ArrayList<String> al=new ArrayList<String>();
+        
+        BasicDBObject first = new BasicDBObject();
+        first.put("idCloud", idcloud);
+        first.put("uuidTemplate",uuidTemplate);
+        first.put("stackName",stackname);
+
+        DB database = this.getDB(dbName);
+        DBCollection collection = database.getCollection("runTimeInfo");
+        try{
+            DBCursor b= collection.find(first).sort(new BasicDBObject("insertTimestamp",-1)).limit(1);
+            System.out.println("query: "+first);
+            while(b.hasNext())
+                al.add(b.next().toString());
+            return al;
+        }catch(Exception e){
+            LOGGER.error("Conditioned Research for collection: "+collection+", resQuery , sortQuery ");
+            return null;
+        }
+        
+        
 
     }
 
@@ -831,8 +932,8 @@ public class DBMongo {
      * @author gtricomi
      */
     public String getFederationToken(String tenant, String user) throws MDBIException {
-        DB database = this.getDB(tenant);
-        DBCollection collection = database.getCollection("credentials");
+        DB database = this.getDB(this.identityDB);
+        DBCollection collection = database.getCollection("Federation_Credential");
 
         BasicDBObject first = new BasicDBObject();
         first.put("federationUser", user);
@@ -984,9 +1085,12 @@ public class DBMongo {
             query.put("idCloud",dcid);
 
             //System.out.println(query);
-            obj = collection.findOne(query);
+            //obj = collection.findOne(query);
+            System.out.println("MONGO QUERY "+query+ " in DB:"+dbName+" for UUID:"+uuid );
+            DBCursor b=collection.find(query).sort(new BasicDBObject("insertTimestamp",-1)).limit(1);
             if(obj!=null)
-                return (String)obj.get("localResourceName");
+                //return (String)obj.get("localResourceName");
+                return b.next().toString();
             else
                 return null;
         }
@@ -1245,7 +1349,7 @@ public String getMapInfo(String dbName, String uuidTemplate) {
 
         query.put("uuidTemplate", uuidTemplate);
         //cursore = collection.find(query).sort(new BasicDBObject("insertTimestamp",-1)).limit(3);  //query utile nel caso in cui il template viene deployato + di una volta
-        cursore = collection.find(query).sort(new BasicDBObject("insertTimestamp",-1));
+        cursore = collection.find(query).sort(new BasicDBObject("insertTimestamp",-1)).limit(1);
         map = new MapInfo();
 
         if (cursore != null) {
@@ -1396,7 +1500,8 @@ public String getMapInfo(String dbName, String uuidTemplate) {
        DBCollection collection = database.getCollection("Federation_Credential");
        BasicDBObject researchField = new BasicDBObject(field, value);
        DBObject risultato = collection.findOne(researchField);
-       String tenantDbName=(String)risultato.get("dbname");
+//06/07/2017 gt: il nome del tenant corrisponde al nome db quindi il campo da ricercare deve essere modificato in federationTenant       
+       String tenantDbName=(String)risultato.get("federationTenant");//("dbname");
        return tenantDbName;
     }
     
@@ -1418,6 +1523,20 @@ public String getMapInfo(String dbName, String uuidTemplate) {
        return tenantName;
     }
     
+    public String getTenantToken(String field,String value, String tenant){
+       DB database = this.getDB(tenant);
+       DBCollection collection = database.getCollection("Federation_Credential");
+       BasicDBObject researchField = new BasicDBObject(field, value);
+       DBObject risultato = collection.findOne(researchField);
+       String tenantName=(String)risultato.get("token");
+       return tenantName;
+    }
+    /**
+     * This function provides the endpoint of the component used inside the architecture.
+     * @param field
+     * @param value
+     * @return 
+     */
     public String getInfo_Endpoint(String field,String value){
        DB database = this.getDB(this.identityDB);
        DBCollection collection = database.getCollection("SystemInfos");
@@ -1451,10 +1570,45 @@ public String getMapInfo(String dbName, String uuidTemplate) {
        
        return ((Number) risultato.get("id")).intValue();//((Number) mapObj.get("autostart")).intValue()//(float) ((double) result.get(v))
     }
+
+    public void insertfedsdnSite(String json, String tenant){
+        
+        DB dataBase = this.getDB(tenant);
+        DBCollection collezione = dataBase.getCollection("fedsdnSite");
+        BasicDBObject obj = (BasicDBObject) JSON.parse(json);
+        obj.append("insertTimestamp", System.currentTimeMillis());
+        collezione.save(obj);
+    }
+    
+    public String getfedsdnSite(String name, String tenant){
+       DB database = this.getDB(tenant);
+       DBCollection collection = database.getCollection("fedsdnSite");
+       BasicDBObject researchField = new BasicDBObject("name", name);
+       DBObject risultato = collection.findOne(researchField);
+       return risultato.toString();
+    }
+     public int getfedsdnSiteID(String name, String tenant){
+       DB database = this.getDB(tenant);
+       DBCollection collection = database.getCollection("fedsdnSite");
+       BasicDBObject researchField = new BasicDBObject("name", name);
+       DBObject risultato = collection.findOne(researchField);
+       
+       return ((Number) risultato.get("id")).intValue();//((Number) mapObj.get("autostart")).intValue()//(float) ((double) result.get(v))
+    }
+     
      
     public void insertfedsdnFednet(String json){
         
         DB dataBase = this.getDB(this.identityDB);
+        DBCollection collezione = dataBase.getCollection("fedsdnFednet");
+        BasicDBObject obj = (BasicDBObject) JSON.parse(json);
+        obj.append("insertTimestamp", System.currentTimeMillis());
+        collezione.save(obj);
+    }
+    
+    public void insertfedsdnFednet(String json, String tenant){
+        
+        DB dataBase = this.getDB(tenant);
         DBCollection collezione = dataBase.getCollection("fedsdnFednet");
         BasicDBObject obj = (BasicDBObject) JSON.parse(json);
         obj.append("insertTimestamp", System.currentTimeMillis());
@@ -1476,6 +1630,23 @@ public String getMapInfo(String dbName, String uuidTemplate) {
        
        return ((Number) risultato.get("id")).intValue();//((Number) mapObj.get("autostart")).intValue()//(float) ((double) result.get(v))
     }
+     
+    public String getfedsdnFednet(String fednet_name, String tenant){
+       DB database = this.getDB(tenant);
+       DBCollection collection = database.getCollection("fedsdnFednet");
+       BasicDBObject researchField = new BasicDBObject("federationTenantName", fednet_name);
+       DBObject risultato = collection.findOne(researchField);
+       return risultato.toString();
+    }
+     public int getfedsdnFednetID(String fednet_name, String tenant){
+       DB database = this.getDB(tenant);
+       DBCollection collection = database.getCollection("fedsdnFednet");
+       BasicDBObject researchField = new BasicDBObject("federationTenantName", fednet_name);
+       DBObject risultato = collection.findOne(researchField);
+       
+       return ((Number) risultato.get("id")).intValue();//((Number) mapObj.get("autostart")).intValue()//(float) ((double) result.get(v))
+    }
+     
     //verificare se la parte dei netsegment và qui o và memorizzata nel DB del tenant
     public void insertfedsdnNetSeg(String json){
         
@@ -1495,6 +1666,31 @@ public String getMapInfo(String dbName, String uuidTemplate) {
     }
      public int getfedsdnNetSegID(String vnetName,String CloudID){
        DB database = this.getDB(this.identityDB);
+       DBCollection collection = database.getCollection("fedsdnNetSeg");
+       BasicDBObject researchField = new BasicDBObject("CloudID", CloudID).append("vnetName", vnetName);
+       DBObject risultato = collection.findOne(researchField);
+       
+       return ((Number) risultato.get("id")).intValue();//((Number) mapObj.get("autostart")).intValue()//(float) ((double) result.get(v))
+    }
+     
+    public void insertfedsdnNetSeg(String json, String tenant){
+        
+        DB dataBase = this.getDB(tenant);
+        DBCollection collezione = dataBase.getCollection("fedsdnNetSeg");
+        BasicDBObject obj = (BasicDBObject) JSON.parse(json);
+        obj.append("insertTimestamp", System.currentTimeMillis());
+        collezione.save(obj);
+    }
+    
+    public String getfedsdnNetSeg(String vnetName,String CloudID, String tenant){
+       DB database = this.getDB(tenant);
+       DBCollection collection = database.getCollection("fedsdnNetSeg");
+       BasicDBObject researchField = new BasicDBObject("CloudID", CloudID).append("vnetName", vnetName);
+       DBObject risultato = collection.findOne(researchField);
+       return risultato.toString();
+    }
+     public int getfedsdnNetSegID(String vnetName,String CloudID, String tenant){
+       DB database = this.getDB(tenant);
        DBCollection collection = database.getCollection("fedsdnNetSeg");
        BasicDBObject researchField = new BasicDBObject("CloudID", CloudID).append("vnetName", vnetName);
        DBObject risultato = collection.findOne(researchField);
@@ -1672,8 +1868,49 @@ public String getMapInfo(String dbName, String uuidTemplate) {
         
     } */ 
     
-    public boolean insertNetTable(String tenant,String jsonTable){
-//        this.insert(tenant, "NetTablesInfo", jsonTable);
-        return true;
+  
+    
+    
+    public String retrieveONEFlowTemplate(String tenant, String ManifestName, String onetempNAME) throws MDBIException {
+        DBObject query = new com.mongodb.BasicDBObject();
+        query.put("masterKey", ManifestName);
+        JSONObject manifest = null;
+        try {
+            manifest = new JSONObject(this.getObj(tenant, "master", query.toString()));
+        } catch (JSONException ex) {
+            LOGGER.error("Impossible create a JSONObject form the Object retrieved by master collection");
+        } catch (MDBIException ex) {
+            LOGGER.error("Impossible create a JSONObject form the Object retrieved by master collection");
+        }
+        query = new com.mongodb.BasicDBObject();
+        query.put("type", "ONE::Beacon::OneFlowTemplate");
+        query.put("nome", onetempNAME);
+
+        JSONArray ja = (JSONArray) manifest.remove("resources");
+        String[] arquer = new String[ja.length()];
+        for (int i = 0; i < ja.length(); i++) {
+            try {
+                arquer[i] = ja.getString(i);
+            } catch (JSONException ex) {
+                LOGGER.error("Error in Manifest resources identification, impossible analyze Manifest stored on Database");
+                throw new MDBIException("Error in Manifest resources identification, impossible analyze Manifest stored on Database");
+            }
+        }
+
+        try {
+            query.put("uuid", new com.mongodb.BasicDBObject("$in", arquer));
+            DBObject result = this.find(tenant, "resources", query);
+            result = ((DBObject) result.get("properties"));
+            result = ((DBObject) result.get("onetemplate"));
+            return result.toString();
+
+        } catch (Exception ex) {
+            LOGGER.error("Impossible create a JSONObject with the oneFlow Template retrieved from Manifest");
+            throw new MDBIException("Impossible create a JSONObject with the oneFlow Template retrieved from Manifest");
+        }
+       
     }
+    
+    
 }
+
